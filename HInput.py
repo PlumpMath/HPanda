d88c04dc79bae26fe64836ec198fd1da43fe230f
hikaruAi <jats00999@yahoo.es>
2015-02-11 21:25:27 -0600

    Moved MouseLook to HCameras and adding here Joystick support

--- HInput.py
+++ HInput.py
@@ -1,66 +1,31 @@
 # ##HPanda Library
 # Input related classes
+#import pygame
+#from direct.showbase import DirectObject
 
-from direct.showbase.ShowBase import ShowBase
-from panda3d.core import Vec3, WindowProperties
+#import os
 
-
-class HMouseLook():
-    def __init__(self, showBase, xFact=500, yFact=500, applyH=True, applyP=True):
-        self.Base = showBase
-        self.xFact = xFact
-        self.yFact = yFact
-        self.centerMouse()
-        self.setTask()
-        self.centerX = base.win.getXSize() / 2
-        self.centerY = base.win.getXSize() / 2
-        self.dH = 0
-        self.dP = 0
-        self.applyH = applyH
-        self.applyP = applyP
-        self.hideMouse()
-
-    def hideMouse(self, value=True):
-        prop = WindowProperties()
-        prop.setCursorHidden(value)
-        base.win.requestProperties(prop)
-
-    def disable(self):
-        self.Base.removeTask("updateTask")
-
-    def enable(self):
-        self.setTask()
-
-    def setTask(self):
-        # if base.mouseWatcherNode.hasMouse():
-        self.Base.addTask(self.updateTask, "updateTask")
-        print "Task Added"
-
-    def updateTask(self, task):
-        try:
-            x = base.mouseWatcherNode.getMouseX()
-            y = base.mouseWatcherNode.getMouseY()
-        except:
-            self.centerMouse()
-            return task.cont
-        if x == 0:
-            self.dH = 0
-        if y == 0:
-            self.dP = 0
-            self.centerMouse()
-        if x == 0 and y == 0:
-            return task.cont
+class HJoystickSensor():
+    def __init__(self,joystickId=0):
+        #print os.getcwd()
+        import pygame #pygame must be in the Main.py directory
+        pygame.init()
+        pygame.joystick.init()
+        if pygame.joystick.get_count()>0:
+            self.id=joystickId
+            self.object=pygame.joystick.Joystick(self.id)
+            self.numButtons=self.object.get_numbuttons()
+            self.numAxes=self.object.get_numaxes()
+            base.taskMgr.add(self.handler,"taskForJoystick_"+self.id)
         else:
-            dt = globalClock.getDt()
-            self.dH = -x * self.xFact * dt
-            self.dP = y * self.xFact * dt
-            if self.applyH:
-                self.Base.camera.setH(self.Base.camera, self.dH)
-            if self.applyP:
-                self.Base.camera.setP(self.Base.camera, self.dP)
-            self.Base.camera.setR(self.Base.render, 0)
-            self.centerMouse()
-            return task.cont
-
-    def centerMouse(self):
-        base.win.movePointer(0, base.win.getXSize() / 2, base.win.getYSize() / 2)
+            print "No Joystick connected"
+    def handler(self,t):
+        for b in range(self.numButtons):
+            if self.object.get_button(b):
+                messenger.send("Joystick_Button_"+str(b))
+        for a in range(self.numAxes):
+            axis=self.object.get_axis(a)
+            if axis!=0:
+                messenger.send("Joystick_Axis_"+str(a),sentArgs[a])
+        return t.cont
+        ##Hats y otras cosas que no uso ahorita
